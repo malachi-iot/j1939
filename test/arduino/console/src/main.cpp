84c7b7107279ce0c3e1d81617ccf0203ac0b93eb
Malachi Burke <malachi.burke@gmail.com>
2023-06-28 18:13:21 -0700

    Brute forcing some submenu support
    Finding some possible limitations in estd::expected implementation

--- test/arduino/console/src/main.cpp
+++ test/arduino/console/src/main.cpp
@@ -36,23 +36,75 @@ public:
 };
 
 
+class Menu;
+class SubMenuItem;
+
+class MenuNavigator
+{
+    friend class Menu;
+    friend class SubMenuItem;
+
+    Menu* top_;
+    Menu* current_;
+
+public:
+    MenuNavigator(Menu* top) :
+        top_{top},
+        current_{top}
+    {}
+
+    Menu* current() const { return current_; }
+    bool up();
+};
+
+
 class Menu
 {
 public:
-    Menu* parent = nullptr;
+    typedef arduino_ostream ostream;
+
+    Menu* parent_ = nullptr;
     std::vector<MenuItem*> items;
 
-    estd::expected<void, estd::errc> activate(int index)
+    Menu() = default;
+    explicit Menu(Menu* parent) : parent_{parent} {}
+
+    expected<void, errc> activate(int index)
     {
         // CTAD not a full option since we're c++11 mostly
         if(index < 0) return unexpected<errc>(errc::invalid_argument);
-        if(index > items.size()) return unexpected<errc>(errc::invalid_argument);
+        // FIX: We expect this brace init list to work, but it doesn't
+        //if(index < 0) return { unexpect_t{}, errc::invalid_argument };
+        if((unsigned)index >= items.size()) return unexpected<errc>(errc::invalid_argument);
 
         items[index]->action();
 
         return {};
     }
+
+    void render(ostream& out)
+    {
+        int i = 0;
+
+        for(const MenuItem* item : items)
+        {
+            out << ++i << ": ";
+            item->render(out);
+            out << endl;
+        }
+    }
 };
+
+
+
+bool MenuNavigator::up()
+{
+    if(current_->parent_ == nullptr) return false;
+
+    current_ = current_->parent_;
+    return true;
+}
+
 
 
 class SyntheticMenuItem : public MenuItem
@@ -77,21 +129,46 @@ public:
 };
 
 
-
-void menu1(Menu* menu)
+class SubMenuItem :  public MenuItem
 {
+    MenuNavigator* navigator_;
+    const char* name_;
+    Menu* menu_;
+
+public:
+    constexpr SubMenuItem(MenuNavigator* navigator, const char* name, Menu* menu = nullptr) :
+        navigator_{navigator},
+        name_{name},
+        menu_{menu}
+    {}
+
+    void render(ostream& out) const override
+    {
+        out << name_;
+    }
+
+    void action() override
+    {
+        cout << name_ << ": " << endl << endl;
+        cout << "0. upward" << endl;
+
+        navigator_->current_ = menu_;
+        menu_->render(cout);
+    }
+};
+
+
+
+void menu1(MenuNavigator* nav)
+{
+    Menu* menu = nav->current();
+
     if(state == States::Entry)
     {
         cout << F("Top level: ") << endl << endl;
         state = States::Running;
-        int i = 0;
 
-        for(const MenuItem* item : menu->items)
-        {
-            cout << ++i << ": ";
-            item->render(cout);
-            cout << endl;
-        }
+        menu->render(cout);
 
         return;
     }
@@ -106,8 +183,6 @@ void menu1(Menu* menu)
 
     switch(c)
     {
-        //case '0':
-
         case 10: break;
 
         case 13:
@@ -115,11 +190,29 @@ void menu1(Menu* menu)
         {
             int selected = -1;
 
-            cout << endl << F("Acting on: ") << input << endl;
+            //cout << endl << F("Acting on: ") << input << endl;
+
             // DEBT: Somewhere we have a string-optimized version of this...
             from_chars(input.data(), input.data() + input.size(), selected);
-            if(selected != -1)
+            if(selected == 0)
+            {
+                if(!nav->up())
+                    cout << "Topmost menu, cannot go up further";
+                else
+                    nav->current()->render(cout);
+            }
+            else if(selected != -1)
+            {
                 menu->activate(selected - 1);
+
+                /* FIX: something's wrong with copy constructor
+                expected<void, errc> r = menu->activate(selected - 1);
+
+                if(r.has_error())
+                {
+
+                }   */
+            }
 
             input.clear();
             break;
@@ -129,17 +222,19 @@ void menu1(Menu* menu)
             // DEBT: Should we have to cast this?
             input += (char)c;
             break;
-
     }
 }
 
-Menu topLevel;
+Menu topLevel, submenu(&topLevel);
 SyntheticMenuItem item1{0}, item2{1};
+MenuNavigator nav(&topLevel);
+SubMenuItem subitem1(&nav, "submenu#1", &submenu);
 
 void setup() 
 {
     topLevel.items.push_back(&item1);
     topLevel.items.push_back(&item2);
+    topLevel.items.push_back(&subitem1);
 
     Serial.begin(115200);
 
@@ -159,5 +254,5 @@ void loop()
 
     cin >> buffer;
     cout << F("You input: ") << buffer << estd::endl; */
-    menu1(&topLevel);
+    menu1(&nav);
 }
