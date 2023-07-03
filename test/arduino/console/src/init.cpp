0acaec4f0312b42b90078af275a620b240b70f1d
Malachi Burke <malachi.burke@gmail.com>
2023-07-03 12:30:49 -0700

    Evidently I forgot some board init
    As per https://learn.adafruit.com/adafruit-feather-m4-can-express/pinouts

--- test/arduino/console/src/init.cpp
+++ test/arduino/console/src/init.cpp
@@ -23,10 +23,15 @@ void init_can(transport& t)
     MCP2515& mcp2515 = t.mcp2515;
     mcp2515.reset();
     can_online = mcp2515.setBitrate(CAN_125KBPS) == MCP2515::ERROR_OK;
-    //mcp2515.setNormalMode();
-    can_online &= mcp2515.setListenOnlyMode() == MCP2515::ERROR_OK;
+    can_online = mcp2515.setNormalMode();
+    //can_online &= mcp2515.setListenOnlyMode() == MCP2515::ERROR_OK;
 #else
     cout << F("SAME5x mode: ");
+
+    pinMode(PIN_CAN_STANDBY, OUTPUT);
+    digitalWrite(PIN_CAN_STANDBY, false); // turn off STANDBY
+    pinMode(4, OUTPUT); // BOOST_ENABLE
+    digitalWrite(4, true); // turn on booster
 
     // start the CAN bus at 125 kbps.  Would go down to
     // 25 kbps where TWAI demo likes to be, but it seems
