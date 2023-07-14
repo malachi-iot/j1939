5bc31e99e6b565c69ce0dd4e80655c9ccff1a51c
Malachi Burke <malachi.burke@gmail.com>
2023-07-13 22:38:38 -0700

    Cleanup

--- test/catch/network-test.cpp
+++ test/catch/network-test.cpp
@@ -25,6 +25,7 @@ struct SyntheticAddressManager
     void reset()
     {
         seed = 0;
+        rotate();
     }
 
     // The most pseudo of psuedo random!
@@ -68,6 +69,11 @@ struct PduDecomposer
     pgns pgn = (pgns) (id.is_pdu1() ? id.range() : _id.range());
 
     estd::span<const uint8_t> payload;
+
+    j1939::layer1::NAME name()
+    {
+        return { payload.data() };
+    }
 
     PduDecomposer(unsigned can_id) :
         id{can_id}, _id{can_id}
@@ -123,8 +129,22 @@ TEST_CASE("Controller Applications (network)")
             v = am.get_candidate();
             REQUIRE(v == saddresses[3]);
         }
+        SECTION("randomness")
+        {
+            std::srand(std::time(nullptr));
+
+            embr::j1939::impl::random_address_manager am;
+
+            for(int i = 100; --i > 0;)
+            {
+                int v = am.get_candidate();
+
+                REQUIRE(v >= 128);
+                REQUIRE(v < 254);
+            }
+        }
     }
-    SECTION("network")
+    SECTION("impl network_ca")
     {
         network_ca impl(name, scheduler);
         const unsigned addr = impl.address_manager().peek_candidate();
@@ -138,6 +158,7 @@ TEST_CASE("Controller Applications (network)")
         pdu<pgns::request> r;
         // announce address claimed
         pdu<pgns::address_claimed> p_claim;
+        frame_type f;
 
         r.payload().pgn((uint32_t)pgns::address_claimed);
         r.destination_address(address_traits::global);
@@ -151,7 +172,37 @@ TEST_CASE("Controller Applications (network)")
         // Ensure loopback is in a good state before we begin
         REQUIRE(t.queue.empty());
 
-        SECTION("CA ignores non-contentious claim")
+        SECTION("CA ignores traffic because it is unstarted")
+        {
+            p_claim.source_address(addr);
+
+            process_incoming(impl, t, frame_traits::create(p_claim));
+
+            REQUIRE(t.queue.empty());
+
+            process_incoming(impl, t, frame_traits::create(r));
+
+            REQUIRE(t.queue.empty());
+        }
+
+        // Initiate network CA, creating announce (claim address)
+        impl.start(t);
+
+        REQUIRE(t.queue.size() == 1);
+        REQUIRE(impl.address() == addr);
+
+        // Pull out initial address claim
+        t.receive(&f);
+
+        PduDecomposer pd{f};
+
+        REQUIRE(pd.pgn == pgns::address_claimed);
+        REQUIRE(pd.id.source_address() == addr);
+        REQUIRE(f.dlc == 8);
+
+        REQUIRE(pd.name() == impl.name());
+
+        SECTION("CA ignores non-contentious claim (synthetic)")
         {
             // This CA will have a look at that request.  Address Claimed only
             // gets responded to if there's a collision, which in this case there
@@ -162,29 +213,18 @@ TEST_CASE("Controller Applications (network)")
         }
         SECTION("CA announces itself")
         {
-            // Initiate network CA, creating announce
-            impl.start(t);
-
-            REQUIRE(!t.queue.empty());
-
-            frame_traits::frame_type frame;
-
-            bool received = t.receive(&frame);
-
-            REQUIRE(received);
-
             // Brute force expected address to compare against
             // DEBT: this much access to given_address a no no
-            p_claim.source_address(*impl.address());
+            p_claim.source_address(addr);
 
             // Because it's a bit of work to convert a native CAN
             // to a PDU, we go the other direction for now
             auto compare_to_frame = frame_traits::create(p_claim);
 
-            REQUIRE(compare_to_frame.id == frame.id);
-            REQUIRE(compare_to_frame.dlc == frame.dlc);
+            REQUIRE(compare_to_frame.id == f.id);
+            REQUIRE(compare_to_frame.dlc == f.dlc);
 
-            PduDecomposer pd{frame.id};
+            pd = f;
 
             REQUIRE(pd.pgn == pgns::address_claimed);
             REQUIRE(pd.id.source_address() == addr);
@@ -193,70 +233,43 @@ TEST_CASE("Controller Applications (network)")
         }
         SECTION("CA contends claim (fully synthetic)")
         {
-            // Initiate network CA, creating announce
-            impl.start(t);
-
             impl.address_manager().reset();
 
             p_claim.source_address(*impl.address());
 
             process_incoming(impl, t, frame_traits::create(p_claim));
 
-            // Contains initial claim followed by contender claim
-            REQUIRE(t.queue.size() == 2);
-
-            frame_type f;
+            // Contains contender claim
+            REQUIRE(t.queue.size() == 1);
 
             t.receive(&f);
 
-            PduDecomposer pd{f};
+            pd = f;
 
             REQUIRE(pd.pgn == pgns::address_claimed);
-            REQUIRE(pd.id.source_address() == addr);
-
-            t.receive(&f);
-
-            pd = f.id;
-
-            REQUIRE(pd.pgn == pgns::address_claimed);
-            // TODO: Not quite ready yet
-            //REQUIRE((unsigned)pd.id.source_address() == addr);
+            REQUIRE((unsigned)pd.id.source_address() == addr);
+            REQUIRE(pd.name() == impl.name());
 
             REQUIRE(!t.receive(&f));
         }
         SECTION("CA contends claim (2 SAs)")
         {
+            // Since contender uses same pseudo algorithm SA generator, its addresses
+            // collide with primary (impl)
             j1939::layer1::NAME contender_name;
             test::names::agricultural_planter<true>::populate(contender_name);
             network_ca contender(contender_name, scheduler);
 
             REQUIRE(!(contender.name() == impl.name()));
 
-            // Initiate primary CA, creating announce
-            impl.start(t);
-
             // Initiate secondary (contender) CA, creating announce also on same address
             contender.start(t);
 
-            // Contains initial claim followed by contender claim
-            REQUIRE(t.queue.size() == 2);
+            // Contains contender claim
+            REQUIRE(t.queue.size() == 1);
 
             // Technically we should have something like two duplicated transports
             // to really simulate all this, but for now we'll fudge it
-            frame_type f;
-
-            // Yank out primary CA address claim
-            REQUIRE(t.receive(&f));
-
-            PduDecomposer pd{f};
-
-            REQUIRE(pd.pgn == pgns::address_claimed);
-            REQUIRE(pd.id.source_address() == addr);
-            REQUIRE(f.dlc == 8);
-
-            embr::j1939::layer1::NAME n(f.payload);
-
-            REQUIRE(n == impl.name());
 
             // Evaluate contender first
             process_incoming(contender, t, f);
@@ -267,9 +280,8 @@ TEST_CASE("Controller Applications (network)")
             t.receive(&f);
 
             pd = f;
-            n = f.payload;
 
-            REQUIRE(n == contender.name());
+            REQUIRE(pd.name() == contender.name());
             REQUIRE(pd.pgn == pgns::address_claimed);
             REQUIRE(pd.id.source_address() == addr);
             REQUIRE(f.dlc == 8);
@@ -280,9 +292,8 @@ TEST_CASE("Controller Applications (network)")
             t.receive(&f);
 
             pd = f;
-            n = f.payload;
 
-            REQUIRE(n == contender.name());
+            REQUIRE(pd.name() == contender.name());
             REQUIRE(pd.pgn == pgns::address_claimed);
             REQUIRE((unsigned)pd.id.source_address() == saddresses[1]);
 
@@ -312,32 +323,9 @@ TEST_CASE("Controller Applications (network)")
             // FIX: This entire test is suspect, since it's indeterminate
             // if on start a CA should issue a request for address claimed
 
-            // DEBT
-            //impl.address.reset();
-
-            impl.start(t);
-
-            REQUIRE(!t.queue.empty());
-
-            frame_type f;
-
             t.receive(&f);
 
             process_incoming(impl, t, f);
-        }
-        SECTION("randomness")
-        {
-            std::srand(std::time(nullptr));
-
-            embr::j1939::impl::random_address_manager am;
-
-            for(int i = 100; --i > 0;)
-            {
-                int v = am.get_candidate();
-
-                REQUIRE(v >= 128);
-                REQUIRE(v < 254);
-            }
         }
     }
 }
