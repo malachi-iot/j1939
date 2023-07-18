bf6ae1e40f1d75a3ada2df27483395347229c4a6
Malachi Burke <malachi.burke@gmail.com>
2023-07-18 10:59:34 -0700

    Prepping ostream specific test area

--- test/arduino/listener/src/main.cpp
+++ test/arduino/listener/src/main.cpp
@@ -1,16 +1,16 @@
 #include <Arduino.h>
 
-#include <estd/istream.h>
-#include <estd/ostream.h>
 #include <estd/string.h>
 
 #include <j1939/pdu.h>
 #include <j1939/pgn.h>
 
+#include <j1939/cas/diagnostic.hpp>
+#include <j1939/ca.hpp>
+
 #include <j1939/data_field/all.hpp>
 
-// DEBT: Eventually we'd like to auto include this
-#include <j1939/units/ostream.h>
+#include <j1939/ostream.h>
 
 #include "transport.h"
 
@@ -19,13 +19,15 @@ using namespace embr::j1939;
 using namespace embr::units::literals;
 
 arduino_ostream cout(Serial);
-arduino_istream cin(Serial);
 
 #ifdef AUTOWP_LIB
 static transport t(10);     // CS pin
 #else
 static transport t;
 #endif
+
+
+diagnostic_ca<transport, arduino_ostream> dca(cout);
 
 
 void setup()
@@ -40,5 +42,11 @@ void setup()
 
 void loop()
 {
+    transport::frame f;
 
+    if(t.receive(&f))
+    {
+        // overflows attiny
+        //process_incoming(dca, t, f);
+    }
 }
---
+++ test/catch/ostream-test.cpp
@@ -0,0 +1,56 @@
+#include <estd/sstream.h>
+
+#include <j1939/ostream.h>
+
+#include "test-data.h"
+
+using ostringstream = estd::detail::basic_ostream<estd::layer1::stringbuf<128>>;
+
+using namespace embr;
+using namespace embr::j1939;
+
+TEST_CASE("ostream")
+{
+    ostringstream out;
+    const auto& out_s = out.rdbuf()->str();
+
+    SECTION("NAME")
+    {
+        test::names::trailer_brake<true>::sparse name1;
+        j1939::layer1::NAME name2;
+
+        name1.populate(name2);
+
+        //out << embr::j1939::put_payload(name2);
+
+        out << j1939::internal::NAME_payload_put{name2};
+
+        REQUIRE(out_s == "ig1 vs=2:0 f=9:0 ecu=0 mc=2047 id=1fffff");
+    }
+    SECTION("pdu_header")
+    {
+        SECTION("pdu1")
+        {
+            j1939::pdu1_header ph{0};
+
+            out << ph;
+        }
+        SECTION("pdu2")
+        {
+            j1939::pdu2_header ph{0};
+
+            out << ph;
+        }
+    }
+    SECTION("data_field")
+    {
+        SECTION("oel")
+        {
+            data_field<pgns::oel> payload;
+
+            out << estd::hex << j1939::internal::payload_put<pgns::oel>{payload};
+
+            REQUIRE(out_s == "ff ff ff ff ff ff ff ff ");
+        }
+    }
+}
