5134025284ad05b47b6c8ad100b78109274dd5c8
Malachi Burke <malachi.burke@gmail.com>
2023-09-12 20:33:04 -0700

    Freshening

---
+++ test/esp32/README.md
@@ -0,0 +1,8 @@
+# Joystick
+
+Listens for GPIO button presses and emits them as BJM1:button1 pressed
+
+# Listener
+
+Listener avoids anything fancy like `embr` services, etc. so as to be the most
+direct and easy example + test to understand
--- test/esp32/joystick/main/ca.h
+++ test/esp32/joystick/main/ca.h
@@ -10,7 +10,7 @@
 // DEBT: I'd prefer nonblocking one right now... I think
 using transport_type = embr::can::esp_idf::twai_transport<true>;
 
-using ostream_type = estd::experimental::ostringstream<256>;
+using ostream_type = estd::layer1::ostringstream<256>;
 
 // DEBT: impl vs non impl here
 using dca_type = embr::j1939::diagnostic_ca<transport_type, ostream_type>;
