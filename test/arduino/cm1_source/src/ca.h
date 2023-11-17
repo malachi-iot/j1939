3d1a961a3a7f4d89785bce346bd344b0d97c5c53
Malachi Burke <malachi.burke@gmail.com>
2023-11-17 13:32:55 -0800

    Percent unit bug workaround + adding in NAME/address acquisition

--- test/arduino/cm1_source/src/ca.h
+++ test/arduino/cm1_source/src/ca.h
@@ -9,6 +9,9 @@
 
 #include <j1939/pdu.h>
 #include <j1939/ca.hpp>
+#include <j1939/NAME/name.h>
+#include <j1939/cas/network.hpp>
+#include <j1939/cas/internal/prng_address_manager.h>
 #include <j1939/data_field/cm1.hpp>
 
 #include "conf.h"
@@ -18,4 +21,13 @@ using scheduler_type = embr::internal::layer1::Scheduler<
     5,
     embr::internal::scheduler::impl::Function<estd::chrono::arduino_clock::time_point> >;
 
-//using ArduinoLightingCommandCa = embr::j1939::impl::lighting_command_ca<transport, scheduler_type>;
+using nca_type = embr::j1939::impl::network_ca<transport,
+    scheduler_type,
+    embr::j1939::internal::prng_address_manager>;
+
+
+// DEBT: Could be better, but we're not too far off
+using proto_name = embr::j1939::layer0::NAME<true,
+    embr::j1939::industry_groups::on_highway,
+    embr::j1939::vehicle_systems::trailer,
+    embr::j1939::function_fields::body_to_vehicle_interface_control>;
--- test/arduino/cm1_source/src/main.cpp
+++ test/arduino/cm1_source/src/main.cpp
@@ -1,5 +1,7 @@
 #include <Arduino.h>
 
+#include <math.h>
+
 #include <estd/chrono.h>
 #include <estd/string.h>
 #include <estd/tuple.h>
@@ -53,6 +55,10 @@ using clock = estd::chrono::arduino_clock;
 diagnostic_ca<transport, arduino_ostream> dca(cout);
 #endif
 
+constexpr embr::word<3> ecu_instance(0);
+
+nca_type nca(proto_name::sparse{0, 0, ecu_instance.value()}, scheduler);
+
 }
 
 void setup()
@@ -66,6 +72,9 @@ void setup()
     cout << F("CM1 Source") << estd::endl;
 
     init_can(t);
+
+    // Try to grab an address right from the start
+    app::nca.start(t);
 }
 
 // presuming 10-bit resolution and no voltage correction curves
@@ -91,17 +100,35 @@ void loop()
 
     // meager thinning data - we need the equivelant of a debounce here,
     // perhaps a true blue smoothing/LP filter
-    if(last_read != v)
+    if(abs(last_read - v) > 2)
     {
+        last_read = v;
+
+        uint32_t v2 = (int32_t)v * 100 / 1024;
+        
         pdu<pgns::cab_message1> pdu;
 
-        adc_percent p(v);
+        if(app::nca.has_address())
+            pdu.source_address(*app::nca.address());
 
-        pdu.requested_percent_fan_speed(p);
+        pdu.destination_address(0xFF);
+
+        // FIX: Pushing adc_percent -> regular percent always gives us
+        // 0 - doesn't seem to be overflow
+        adc_percent p(v);
+        //slot_traits<slots::SAEpc03>::type p2(p);
+        embr::units::percent<uint32_t> p3(v2);
+        slot_traits<slots::SAEpc03>::type p2(p3);
+
+        //pdu.requested_percent_fan_speed(p);
+        pdu.requested_percent_fan_speed(p2);
+        //pdu.requested_percent_fan_speed(p3);
 
         traits::send(t, pdu);
 
-        cout << F("reading: ") << embr::put_unit(mv2) << endl;
+        cout << F("reading: ") << embr::put_unit(mv2) << F("/");
+        cout << p.count() << F("/");
+        cout << embr::put_unit(p3) << endl;
     }
 
     transport::frame f;
@@ -111,6 +138,7 @@ void loop()
 #if CONFIG_DIAGNOSTIC_CA
         process_incoming(app::dca, t, f);
 #endif
+        process_incoming(app::nca, t, f);
     }
 
     // Does use about 6 bytes less memory on AVR...
