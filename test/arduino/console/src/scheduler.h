ea10482de2a25ea54d9b4c7ca4f14d73f1e516e4
Malachi Burke <malachi.burke@gmail.com>
2024-03-06 09:27:24 -0800

    Patched M4 compilation
    I expect this to work, but untested at this time

--- test/arduino/console/src/scheduler.h
+++ test/arduino/console/src/scheduler.h
@@ -1,13 +1,13 @@
 #include <estd/chrono.h>
 
+#include <embr/scheduler.hpp>
+
 #ifdef ESP_PLATFORM
 
 // DEBT: Even though steady clock scheduler by default works for esp,
 // the time_point/duration math between std::chrono and estd::chrono falls down,
 // particularly in the case of operator +
 
-
-#include <embr/scheduler.hpp>
 
 using scheduler_impl_type = embr::internal::scheduler::impl::Function<estd::chrono::freertos_clock::time_point>;
 
@@ -16,7 +16,20 @@ using scheduler_type =
     estd::layer1::vector<typename scheduler_impl_type::value_type, 10>,
     scheduler_impl_type>;
 
+#elif ADAFRUIT_FEATHER_M4_CAN
 
+// 06MAR24 MB UNTESTED
+// Once this tests OK, combine ESP32 above and AVR below into this and
+// test them too  
+
+using scheduler_impl_type =
+    embr::internal::scheduler::impl::Function<estd::chrono::
+    arduino_clock::time_point>;
+
+using scheduler_type = 
+    embr::internal::Scheduler<
+    estd::layer1::vector<typename scheduler_impl_type::value_type, 10>,
+    scheduler_impl_type>;
 
 #else
 
@@ -31,7 +44,6 @@ using steady_clock = arduino_clock;
 }}
 
 #include <embr/platform/arduino/scheduler.h>
-#include <embr/scheduler.hpp>
 
 // DEBT: I think scheduler's just about ready to emerge from internal namespace
 // ~100 bytes for scheduler on Pro micro
