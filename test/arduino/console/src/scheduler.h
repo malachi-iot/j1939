87b82c985b720ef35dbf98a67224a5bc69d47a3a
Malachi Burke <malachi.burke@gmail.com>
2024-03-06 10:19:58 -0800

    AVR and M4 test OK

--- test/arduino/console/src/scheduler.h
+++ test/arduino/console/src/scheduler.h
@@ -7,46 +7,22 @@
 // DEBT: Even though steady clock scheduler by default works for esp,
 // the time_point/duration math between std::chrono and estd::chrono falls down,
 // particularly in the case of operator +
+// NOTE: Likely ESP is happy with arduino_clock too, but I just like using
+// the freertos clock in this circumstance
 
-
-using scheduler_impl_type = embr::internal::scheduler::impl::Function<estd::chrono::freertos_clock::time_point>;
-
-using scheduler_type = 
-    embr::internal::Scheduler<
-    estd::layer1::vector<typename scheduler_impl_type::value_type, 10>,
-    scheduler_impl_type>;
-
-#elif ADAFRUIT_FEATHER_M4_CAN
-
-// 06MAR24 MB UNTESTED
-// Once this tests OK, combine ESP32 above and AVR below into this and
-// test them too  
 
 using scheduler_impl_type =
-    embr::internal::scheduler::impl::Function<estd::chrono::
-    arduino_clock::time_point>;
-
-using scheduler_type = 
-    embr::internal::Scheduler<
-    estd::layer1::vector<typename scheduler_impl_type::value_type, 10>,
-    scheduler_impl_type>;
+    embr::internal::scheduler::impl::Function<estd::chrono::freertos_clock::time_point>;
 
 #else
 
-// DEBT: Not sure how I'm feeling about it.  I removed steady clock alias a little
-// while ago from estd in this context.  Do I miss it?
-// FIX: Because of this DEBT, we can't compile on esp32 because its steady_clock
-// does exist as an alias to std::system clock
-namespace estd { namespace chrono {
-
-using steady_clock = arduino_clock;
-
-}}
-
-#include <embr/platform/arduino/scheduler.h>
-
-// DEBT: I think scheduler's just about ready to emerge from internal namespace
-// ~100 bytes for scheduler on Pro micro
-using scheduler_type = embr::internal::layer1::Scheduler<10>;
+using scheduler_impl_type =
+    embr::internal::scheduler::impl::Function<estd::chrono::arduino_clock::time_point>;
 
 #endif
+
+using scheduler_type = 
+    embr::internal::Scheduler<
+    estd::layer1::vector<typename scheduler_impl_type::value_type, 10>,
+    scheduler_impl_type>;
+
